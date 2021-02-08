d21c6d121fb99601c87c0a06e5d34b5267706a18
Jorge Soto <djsmoothzouk@gmail.com>
2021-02-08 16:07:36 -0600

    removed .env, .txt, and Link from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, } from "react-router-dom";
 import Nav from "./components/Nav";
 import React from "react";
 import computerBuilder from "./pages/computerBuilder"
