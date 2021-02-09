ca7b512c8150264c61a8bc0637bac8a66858c1c5
Alexander Chaconas <alexander.chaconas@gmail.com>
2021-02-09 13:31:27 -0500

    update

--- src/components/SortFilter.js
+++ src/components/SortFilter.js
@@ -1,18 +1,18 @@
 import React from "react"
-import BuildConfigCard from "./BuildConfigCard"
+import BuildConfigCard from "./card/index"
 
 const SortFilter = (props) => {
     const storageData = [
     {name: "AMD 500GB", 
-price: 600, spec: "500 GB"},
-{name: "AMD 400GB", price: 700, spec: "400 GB"}]
+price: 800, spec: "500 GB"},
+{name: "AMD 400GB", price: 900, spec: "400 GB"}]
 const [showStatus, setShowStatus] = React.useState(false)
 const [filteredArr, setFilteredArr] = React.useState(storageData)
 const [checkStatus, setCheckStatus] = React.useState(false)
 const [checkValue, setCheckValue] = React.useState("")
 
 
-const handleCheck = (event) => { 
+const handleFilter = (event) => { 
 if (checkStatus === false) {
   setCheckStatus(true);
   setCheckValue(event.target.value);
@@ -21,15 +21,41 @@ if (checkStatus === false) {
 }
 }
 
+const sortHigh = (event) => {
+  let newArr = [...filteredArr];
+    newArr.sort(
+      (a, b) => parseFloat(b.price) - parseFloat(a.price)
+    );
+    setFilteredArr(newArr);
+  };
+
+const sortLow = (event) => {
+  let newArr = [...filteredArr];
+    newArr.sort(
+      (a, b) => parseFloat(a.price) - parseFloat(b.price)
+    );
+    setFilteredArr(newArr);
+  };
 
 
 const dropdown = () => {
-    return(
-    <form>
-      <p>AMD 500GB</p><input onChange= {handleCheck} type="checkbox" value="AMD 500GB"/>
-      <p>AMD 400GB</p><input onChange= {handleCheck} type="checkbox" value="AMD 400GB"/>
-      <button onClick = {handleStorage} type="submit">Filter</button>
-    </form>)
+    return (
+      <form>
+        <span>Storage: AMD 500GB</span>
+        <input onChange={handleFilter} type="checkbox" value="AMD 500GB" />
+        <span>AMD 400GB</span>
+        <input onChange={handleFilter} type="checkbox" value="AMD 400GB" />
+        <br />
+        <span>Price: Low to High</span>
+        <input onChange={sortLow} type="checkbox"/>
+        <span>Price: High to Low</span>
+        <input onChange={sortHigh} type="checkbox" />
+        <br />
+        <button onClick={handleStorage} type="submit">
+          GO
+        </button>
+      </form>
+    );
 }
 
 
@@ -42,14 +68,14 @@ const handleClick = () => {
 const handleStorage = (event) => {
 event.preventDefault()
 const newArr = filteredArr.filter((storage) =>  storage.name === checkValue)
- checkStatus? setFilteredArr(newArr) : setFilteredArr(storageData)
+ checkStatus? setFilteredArr(newArr) : setFilteredArr(filteredArr)
  setShowStatus(false)
 }
 
 
 return (
     <div className="sortFilter">
-<button onClick={handleClick}>Filter/Sort</button>
+<button onClick={handleClick}>Filter</button>
 {showStatus? dropdown(): filteredArr.map((data, index) => {
             const {name, price, spec} = data;
             return (
