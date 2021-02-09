54ca3a31adae9d24452be10e34e99703daf7fa3a
Andrew <andrew.mk4@yahoo.com>
2021-02-09 10:18:27 -0800

    BuildForm

--- src/pages/buildForm.js
+++ src/pages/buildForm.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Gpus from "../data/gpus"
+import Processors from "../data/processors"
+import PowerSupplys from "../data/powerSupplys"
+import Memorys from "../data/memorys"
+import Motherboards from "../data/motherboards"
+import Storages from "../data/storages"
 
 const BuildForm = (props) => {
   const Build = {
@@ -22,25 +28,37 @@ const BuildForm = (props) => {
   const handleChange = (event) => {
     const value = event.target.value
     const name = event.target.name
-  
-  const gpus = [{}, { name: "NEW ASUS GeForce GTX1070ti", price: 100, spec: "NEW 8GB"}]  
-  setFormData({ ...formData, [name]: gpus[parseInt(value)] });
-  
-  const memorys = [{}, { name: "Crucial Ballistix 8GB x 2 RGB", price: 100, spec: "16GB" }]
-  setFormData({ ...formData, [name]: memorys[parseInt(value)] });
- 
-  const motherboards = [{}, { name: "ASRock x570m pro 4", price: 200, spec: "128GB Max RAM" }]
-  setFormData({ ...formData, [name]: motherboards[parseInt(value)] });
-  
-  const powerSupplys = [{}, { name: "EVGA 600", price: 60, spec: "600w" }]
-  setFormData({ ...formData, [name]: powerSupplys[parseInt(value)] });
+    switch(name) {
+      case "gpu":
+        setFormData({...formData, [name]: Gpus[value]})
+        break
+      case "memory":
+        setFormData({...formData, [name]: Memorys[value]})
+        break
+      case "motherboard":
+        setFormData({...formData, [name]: Motherboards[value]})
+        break
+      case "powerSupply":
+        setFormData({...formData, [name]: PowerSupplys[value]})
+        break
+      case "processor":
+        setFormData({...formData, [name]: Processors[value]})
+        break
+      case "storage":
+        setFormData({...formData, [name]: Storages[value]})
+        break
+      default:
+        break
+    }}
 
-  const processors = [{}, { name: "AMD Ryzen 7 3700", price: 299, spec: "8 core" }]
-  setFormData({ ...formData, [name]: processors[parseInt(value)] });
-  
-  const storages = [{}, { name: "WD 1TB SSD", price: 100, spec: "1TB" }]
-  setFormData({ ...formData, [name]: storages[parseInt(value)] });
-  };
+  const renderOptions = (arr) => {
+    return arr.map((item, index) => {
+      console.log(item)
+      return (
+        <option key={index} value={index}>{item.name}</option>
+      )
+    })
+  }
 
   return (
     <>
@@ -48,72 +66,42 @@ const BuildForm = (props) => {
         <label>
           GPU:
           <select value={formData.gpu.name} onChange={handleChange} name="gpu" price="price" spec="spec">
-            <option value="gpu">GPU</option>
-            <option value="1">GPU1</option>
-            <option value="2">GPU2</option>
-            <option value="3">GPU3</option>
-            <option value="4">GPU4</option>
-            <option value="5">GPU5</option>
+            {renderOptions(Gpus)}
           </select>  
         </label>
         <br />
         <label>
           Memory:
           <select value={formData.memory.name} onChange={handleChange} name="memory" price="price" spec="spec">
-            <option value="memory">Memory</option>
-            <option value="1">Memory1</option>
-            <option value="2">Memory2</option>
-            <option value="3">Memory3</option>
-            <option value="4">Memory4</option>
-            <option value="5">Memory5</option>
+            {renderOptions(Memorys)}
           </select>
         </label>
         <br />
         <label>
           Motherboard:
           <select value={formData.motherboard.name} onChange={handleChange} name="motherboard" price="price" spec="spec">
-            <option value="motherboard">Motherboard</option>
-            <option value="1">motherboard1</option>
-            <option value="2">Motherboard2</option>
-            <option value="3">Motherboard3</option>
-            <option value="4">Motherboard4</option>
-            <option value="5">Motherboard5</option>
+            {renderOptions(Motherboards)}
           </select>
         </label>
         <br />
         <label>
           Power Supply:
           <select value={formData.powerSupply.name} onChange={handleChange} name="powerSupply" price="price" spec="spec">
-            <option value="powerSupply">Power Supply</option>
-            <option value="1">Power Supply1</option>
-            <option value="2">Power Supply2</option>
-            <option value="3">Power Supply3</option>
-            <option value="4">Power Supply4</option>
-            <option value="5">Power Supply5</option>
+            {renderOptions(PowerSupplys)}
           </select>
         </label>
         <br />
         <label>
           Processor:
           <select value={formData.processor.name} onChange={handleChange} name="powerSupply" price="price" spec="spec">
-            <option value="processor">Processor</option>
-            <option value="1">Processor1</option>
-            <option value="2">Processor2</option>
-            <option value="3">Processor3</option>
-            <option value="4">Processor4</option>
-            <option value="5">Processor5</option>
+            {renderOptions(Processors)}
           </select>
         </label>
         <br />
         <label>
           Storage:
           <select value={formData.storage.name} onChange={handleChange} name="storage" price="price" spec="spec">
-            <option value="storage">Storage</option>
-            <option value="1">Storage1</option>
-            <option value="2">Storage2</option>
-            <option value="3">Storage3</option>
-            <option value="4">Storage4</option>
-            <option value="5">Storage5</option>
+            {renderOptions(Storages)}
           </select>
         </label>
         <br />
