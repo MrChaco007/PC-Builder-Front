2264f5ceeb9fb23b4a0f4aa394a5c4cda50c4eee
Andrew <andrew.mk4@yahoo.com>
2021-02-11 16:48:49 -0800

    Team Page Final Draft

--- src/pages/teamPage.js
+++ src/pages/teamPage.js
@@ -4,25 +4,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 import {faBriefcase} from "@fortawesome/free-solid-svg-icons"
 import { Card, Button, Accordion } from "react-bootstrap";
+import "./teamPage.css"
 import {Link} from "react-router-dom"
 const TeamPage = () => {
+
   return (
     <div className="team">
+      <h1 className="teamHead">Team Page</h1>
+      <div className="cardsContainer"> 
       <Card className="card" style={{ width: "18rem", textAlign: "center" }}>
         <div className="hover-1">
-        <div className="hover-overlay">
         <Card.Img
           variant="top"
           img src="https://res.cloudinary.com/dvnl2s9um/image/upload/v1608745815/15672988_10211212030552102_7722795121537872527_n_pkzqgc.jpg"
           alt=""
         />
-        </div>
         <div className="hover-1-content">
-          <h3 className="hover-1-title"><span>Adrian Chaconas</span></h3>
+          <h3 className="hover-1-title"><span>Alex Chaconas</span></h3>
         </div>
         </div>
         <Card.Body>
-          {/* <Card.Title style={{ fontSize: "35px" }}>Alex Chaconas</Card.Title> */}
           <a className="icon-link" href="https://github.com/MrChaco007">
             <FontAwesomeIcon className="icon" icon={faGithubSquare} size="2x" />
           </a>
@@ -40,14 +41,16 @@ const TeamPage = () => {
           >
             <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x" />
           </a>
-          <Accordion defaultActiveKey="1">
-            <Card>
-              <Accordion.Toggle as={Button} variant="dark" eventKey="0">
-                Learn more about Alex
+          <Accordion defaultActiveKey="0" id="card1">
+            <Card className="alex">
+              <Accordion.Toggle className="learn" as={Button} variant="dark" eventKey="0" data-parent="card1">
+                Learn more about <span className=
+                "colorName">Alex</span>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body style={{ fontSize: "16px" }}>
-                  Alex is a software developer with 4 years of marketing and
+                  <br />
+                  <p className="infoPar"><span className="firstLetter">A</span>lex is a software developer with 4 years of marketing and
                   communications experience. He combines his rapidly evolving
                   coding potential with his exceptional soft skills to not only
                   find solutions to technical problems but also ensure
@@ -58,7 +61,8 @@ const TeamPage = () => {
                   and to become a reliable asset for his team. My marketing
                   experience enables him to see how even the most robust
                   back-end logic is futile without an intuitive and appealing
-                  front-end design.
+                  front-end design.</p>
+                  <br />
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
@@ -66,12 +70,16 @@ const TeamPage = () => {
         </Card.Body>
       </Card>
       <Card className="card" style={{ width: "18rem", textAlign: "center" }}>
+        <div className="hover-1">
         <Card.Img
           variant="top"
           src="https://res.cloudinary.com/dfltkqotn/image/upload/c_fill,h_254,w_254/v1612906104/IMG_6547_lvytbc_a8stcg.jpg"
         />
+        <div className="hover-1-content">
+          <h3 className="hover-1-title"><span>Adrian Mendez</span></h3>
+        </div>
+        </div>
         <Card.Body>
-          <Card.Title style={{ fontSize: "35px" }}>Adrian Mendez</Card.Title>
           <a className="icon-link" href="https://github.com/adrianmendez03">
             <FontAwesomeIcon className="icon" icon={faGithubSquare} size="2x" />
           </a>
@@ -89,25 +97,19 @@ const TeamPage = () => {
           >
             <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x" />
           </a>
-          <Accordion defaultActiveKey="1">
+          <Accordion defaultActiveKey="1" id="card2">
             <Card>
-              <Accordion.Toggle as={Button} variant="dark" eventKey="0">
-                Learn more about Adrian
+              <Accordion.Toggle as={Button} variant="dark" eventKey="0" data-parent="card2">
+                Learn more about <span className="colorName">Adrian</span>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body style={{ fontSize: "16px" }}>
-                  Alex is a software developer with 4 years of marketing and
-                  communications experience. He combines his rapidly evolving
-                  coding potential with his exceptional soft skills to not only
-                  find solutions to technical problems but also ensure
-                  interpersonal conflict does not get in the way.
-                  <br />
-                  <br />
-                  He is motivated by his appetite to get better at what he does
-                  and to become a reliable asset for his team. My marketing
-                  experience enables him to see how even the most robust
-                  back-end logic is futile without an intuitive and appealing
-                  front-end design.
+                <br />
+                <p className="infoPar"><span className="firstLetter">F</span>ull Stack Engineer who enjoys solving complex problems with effective solutions. 
+                <br />
+                <br />
+                Determined to write clean, precise code, and design efficient systems. Inspired by an ancestry of tradesmen to build expressive and intuitive products in the digital space.</p>
+                <br />
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
@@ -115,12 +117,16 @@ const TeamPage = () => {
         </Card.Body>
       </Card>
       <Card className="card" style={{ width: "18rem", textAlign: "center" }}>
+        <div className="hover-1">
         <Card.Img
           variant="top"
           src="https://res.cloudinary.com/dfltkqotn/image/upload/v1612905073/1608763532612_z2rmzr.jpg"
         />
+        <div className="hover-1-content">
+          <h3 className="hover-1-title"><span>Jorge Soto</span></h3>
+        </div>
+        </div>
         <Card.Body>
-          <Card.Title style={{ fontSize: "35px" }}>Jorge Soto</Card.Title>
           <a className="icon-link" href="https://github.com/acurunner79">
             <FontAwesomeIcon className="icon" icon={faGithubSquare} size="2x" />
           </a>
@@ -138,17 +144,19 @@ const TeamPage = () => {
           >
             <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x" />
           </a>
-          <Accordion defaultActiveKey="1">
+          <Accordion defaultActiveKey="2">
             <Card>
               <Accordion.Toggle as={Button} variant="dark" eventKey="0">
-                Learn more about Jorge
+                Learn more about <span className="colorName">Jorge</span>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body style={{ fontSize: "16px" }}>
-                Aspiring Full-stack web developer and professional dancer who is always looking for ways to level up in all aspects. 
+                <br />  
+                <p className="infoPar"><span className="firstLetter">A</span>spiring Full-stack web developer and professional dancer who is always looking for ways to level up in all aspects. 
                 <br />
                 <br />
-                Passionate about applying structure in my everyday life, making me more efficient. Eager to create functional products shaped around consumer wants and needs influenced by industry-based feedback and research.
+                Passionate about applying structure in my everyday life, making me more efficient. Eager to create functional products shaped around consumer wants and needs influenced by industry-based feedback and research.</p>
+                <br />
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
@@ -156,12 +164,16 @@ const TeamPage = () => {
         </Card.Body>
       </Card>
       <Card className="card" style={{ width: "18rem", textAlign: "center" }}>
+        <div className="hover-1">
         <Card.Img
           variant="top"
           src="https://avatars.githubusercontent.com/u/75413323?s=400&u=9beccb5dd76ccdf3c7aaba2d1f120856285b26a4&v=4"
         />
+        <div className="hover-1-content">
+          <h3 className="hover-1-title"><span>Andrew Arias</span></h3>
+        </div>
+        </div>
         <Card.Body>
-          <Card.Title style={{ fontSize: "35px" }}>Andrew Arias</Card.Title>
           <a className="icon-link" href="https://github.com/lta4">
             <FontAwesomeIcon className="icon" icon={faGithubSquare} size="2x" />
           </a>
@@ -179,23 +191,27 @@ const TeamPage = () => {
           >
             <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x" />
           </a>
-          <Accordion defaultActiveKey="1">
+          <Accordion defaultActiveKey="3">
             <Card>
               <Accordion.Toggle as={Button} variant="dark" eventKey="0">
-                Learn more about Andrew
+                Learn more about <span className="colorName">Andrew</span>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body style={{ fontSize: "16px" }}>
-                  Andrew is a Software Developer who was born and raised in Los Angeles. Now residing in San Diego, he has committed 6 years to the Financial Service Industry and another 6 too the Food and Beverage Hospitality Industry.
+                  <br />
+                  <p className="infoPar"><span className="firstLetter">
+                  A</span>ndrew is a Software Developer who was born and raised in Los Angeles. Now residing in San Diego, he has committed 6 years to the Financial Service Industry and Food and Beverage Industry. 
                   <br />
                   <br />
-                  With his newly aquired skills in HTML, CSS, JavaScript, and React, he strives to implement a soft, clean touch with great responsiveness and color pop. Simple and reliability are dynamics he enjoys, in order make a proper statement. It really is the simple things that stand out!
+                  With his newly aquired skills in HTML, CSS, JavaScript, and React, he strives to implement a soft, clean touch with great responsiveness and color pop. Simple and reliability are dynamics he enjoys, in order make a proper statement. It really is the simple things that stand out!</p>
+                  <br />
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
           </Accordion>
         </Card.Body>
       </Card>
+      </div>
     </div>
   );
 };
